b901266badfe5a2dbb226ccf4a7d5ed9f9480ed2
ThuvaThaya <thuvathaya1919@gmail.com>
2021-09-22 18:08:13 +0530

    latest commit

--- app/src/main/java/com/example/hrb/BookingActivity.java
+++ app/src/main/java/com/example/hrb/BookingActivity.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BookingActivity extends AppCompatActivity {
 
+    EditText chkInDateEditTxt,chkInTimeEditTxt,chkOutDateEditTxt,chkOutTimeEditTxt,roomTitleEditTxt,noOfRoomsEditTxt,
+            mobileEditTxt,emailEditTxt,discountEditTxt,fareEditTxt;
+    Button btnBook, btnCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
